2921283d0e17c727f13b767c3e7d520e29e94dbb
Larry <long05012001@gmail.com>
2024-04-06 22:48:55 +0700

    add feature 1

--- app/src/main/java/com/longkd/sampleci_cd/MainActivity.kt
+++ app/src/main/java/com/longkd/sampleci_cd/MainActivity.kt
@@ -3,12 +3,17 @@ package com.longkd.sampleci_cd
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.longkd.sampleci_cd.ui.theme.SampleCI_CDTheme
 
@@ -17,6 +22,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SampleCI_CDTheme {
+                enableEdgeToEdge()
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -31,10 +37,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column() {
+        Button(modifier = Modifier.fillMaxWidth(), onClick = { /*TODO*/ }) {
+            Text(text = name, textAlign = TextAlign.Center)
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
